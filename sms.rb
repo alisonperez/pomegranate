ce66868590eabd11efba83a9b84e9c8ceeab0cb9
Mike McKay <mikeymckay@gmail.com>
2013-05-08 02:26:15 +0000

    Getting set for deployment

--- sms.rb
+++ sms.rb
@@ -3,7 +3,8 @@ require 'sinatra'
 require 'rest-client'
 require 'cgi'
 
-get '/post_to_google' do
-  response = RestClient.post "https://docs.google.com/forms/d/1gth6LVg8fQXs8oWvV_4dw2u767Z-XNxWuHLX8oyN0Yw/formResponse", "entry.980814057" => CGI.escape(params["phone"]) , "entry.191252353" => CGI.escape(params["text"])
-  puts response.to_str
+get '/ghana_may_2013' do
+  RestClient.post "https://docs.google.com/forms/d/1gth6LVg8fQXs8oWvV_4dw2u767Z-XNxWuHLX8oyN0Yw/formResponse",
+    "entry.980814057" => CGI.escape(params["phone"]),
+    "entry.191252353" => CGI.escape(params["text"])
 end
