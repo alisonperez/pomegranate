a1c17fe2a056068169ca608853a519415bebe2eb
Mike McKay <mikeymckay@gmail.com>
2013-05-10 20:08:55 +0000

    Trying to handle multiple instances

--- sms.rb
+++ sms.rb
@@ -3,8 +3,21 @@ require 'sinatra'
 require 'rest-client'
 require 'cgi'
 
-get '/ghana_may_2013' do
+get '/received_ghana_may_2013' do
   RestClient.post "https://docs.google.com/forms/d/1gth6LVg8fQXs8oWvV_4dw2u767Z-XNxWuHLX8oyN0Yw/formResponse",
-    "entry.980814057" => CGI.escape(params["phone"]),
-    "entry.191252353" => CGI.escape(params["text"])
+    "entry.980814057" => params["phone"],
+    "entry.191252353" => params["text"]
+end
+
+get '/log_sent_message' do
+  response['Access-Control-Allow-Origin'] = '*'
+  File.open("/var/www/pomegranate/sent_message.log", 'a') do |file| 
+    file.write("#{Time.now.to_s}: #{params['phone']} -> #{params['text']}\n")
+  end
+  "#{Time.now.to_s}: #{params['phone']} -> #{params['text']}\n"
+end
+
+get '/log' do
+  response['Access-Control-Allow-Origin'] = '*'
+  IO.read("/var/www/pomegranate/sent_message.log").split("\n").reverse().join("<br/>")
 end
